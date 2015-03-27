62a51d1f2a4229ad9834781bfd47d25d7620a57c
Knoobie <Knoobie@gmx.de>
2015-03-27 22:31:30 +0100

    added Picture, Titles, Franchises, Websites, Releases and Related Albums Support

--- src/main/java/de/knoobie/project/fuko/database/domain/Product.java
+++ src/main/java/de/knoobie/project/fuko/database/domain/Product.java
@@ -1,6 +1,8 @@
 package de.knoobie.project.fuko.database.domain;
 
 import de.knoobie.project.fuko.database.domain.msc.MSCClannadMeta;
+import de.knoobie.project.fuko.database.utils.VGMdbProductModifier;
+import de.knoobie.project.nagisa.gson.model.bo.VGMdbProduct;
 import de.knoobie.project.nagisa.gson.model.bo.enums.VGMdbProductType;
 import java.io.Serializable;
 import java.sql.Date;
@@ -9,39 +11,58 @@ import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-public @Getter @Setter class Product extends MSCClannadMeta implements Serializable {
+public @Getter
+@Setter
+class Product extends MSCClannadMeta implements Serializable {
 
-  @Basic
-  @Column(nullable = true)
-  private String organizations;
+    @Basic
+    @Column(nullable = true)
+    private String organizations;
 
-  @Basic
-  @Column(nullable = true)
-  private String releaseDate;
-  
-  @Basic
-  @Column(nullable = true)
-  private VGMdbProductType productType;
-  
-  
-  @ManyToMany(targetEntity = ProductMerchandise.class)
-  private List<ProductMerchandise> titles = new ArrayList<>();
-  
-  @ManyToMany(targetEntity = ProductMerchandise.class)
-  private List<ProductMerchandise> francises = new ArrayList<>();
-  
-  @ManyToMany(targetEntity = ProductMerchandise.class)
-  private List<ProductMerchandise> releases = new ArrayList<>();
-  
-  @ManyToMany(targetEntity = Album.class)
-  private List<Album> relatedAlbums = new ArrayList<>();
+    @Basic
+    @Column(nullable = true)
+    private String releaseDate;
 
-  public Product() {
+    @Basic
+    @Column(nullable = true)
+    private VGMdbProductType productType;
 
-  }
+    @OneToOne(optional = true, targetEntity = Picture.class)
+    private Picture picture;
+
+    @ManyToMany(targetEntity = Product.class)
+    @JoinTable(name = "product_titles")
+    private List<Product> titles = new ArrayList<>();
+
+    @ManyToMany(targetEntity = Product.class)
+    @JoinTable(name = "product_franchises")
+    private List<Product> franchises = new ArrayList<>();
+
+    @ManyToMany(targetEntity = Website.class)
+    @JoinTable(name = "product_websites")
+    private List<Website> websites = new ArrayList<>();
+
+    @OneToMany(targetEntity = ProductRelease.class, mappedBy = "product")
+    @JoinTable(name = "product_releases")
+    private List<ProductRelease> releases = new ArrayList<>();
+
+    @ManyToMany(targetEntity = Album.class)
+    @JoinTable(name = "product_related_abums")
+    private List<Album> relatedAlbums = new ArrayList<>();
+
+    public Product() {
+
+    }
+
+    public static Product getFromVGMDB(VGMdbProduct product) {
+        return VGMdbProductModifier.transformVGMdbProduct(product, false);
+    }
 }
